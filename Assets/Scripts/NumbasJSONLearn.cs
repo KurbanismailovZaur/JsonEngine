2cd94b5ffbefd9087821e1dbf4b76e228a65dde6
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2018-01-11 13:32:49 +0300

    Example writed

--- Assets/Scripts/NumbasJSONLearn.cs
+++ Assets/Scripts/NumbasJSONLearn.cs
@@ -1,12 +1,29 @@
-﻿using Numba.Data.Json.Engine.DataTypes;
+﻿using Numba.Data.Json.Engine;
+using Numba.Data.Json.Engine.DataTypes;
+using Numba.Data.Json.Engine.Extentions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NumbasJSONLearn : MonoBehaviour 
+public class NumbasJSONLearn : MonoBehaviour
 {
     private void Start()
     {
-        
+        JsonObject jObject = new JsonObject() { { "int", 1 } };
+        jObject.SetInt("int", null);
+        string jsonData = jObject.ToString();
+        print(jsonData);
+
+        JsonObject parsedJObject = Json.Parse<JsonObject>(jsonData);
+        print(parsedJObject);
+
+        if (parsedJObject.IsNullField("int"))
+        {
+            Debug.Log("Sorry, field with name \"int\" is null");
+        }
+        else
+        {
+            int value = parsedJObject.GetNumber("int").ToInt();
+        }
     }
 }
