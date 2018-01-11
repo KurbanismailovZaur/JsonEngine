d9049254534a5ca323fad2a8484897413100fd68
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2018-01-11 06:26:53 +0300

    Added scene for learning

---
+++ Assets/Scripts/NumbasJSONLearn.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumbasJSONLearn : MonoBehaviour 
+{
+    private void Start()
+    {
+        
+    }
+}
